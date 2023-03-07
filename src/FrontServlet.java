b8ea1c64a5e8e3f7253697d9667c8c5a2859e549
andraina1901 <randraina@yahoo.com>
2023-03-07 08:46:55 +0300

    sprint2 1.0

--- src/FrontServlet.java
+++ src/FrontServlet.java
@@ -2,7 +2,9 @@ package ETU1901.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
+import ETU1901.framework.Mapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +12,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import objet.Utilitaire;
 
 public class FrontServlet extends HttpServlet {
+
+    HashMap<String,Mapping> MappingUrls;
 
     public void ProcessRequest(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
         String url = req.getServletPath();
@@ -25,6 +29,14 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         this.ProcessRequest(req, resp);
+    }
+
+    public HashMap<String, Mapping> getMappingUrls() {
+        return MappingUrls;
+    }
+
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+        MappingUrls = mappingUrls;
     }
     
 }
