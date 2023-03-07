7596cfc7132d29cd3ac2a42cff12e4a4b32ca501
andraina1901 <randraina@yahoo.com>
2023-03-07 08:37:23 +0300

    sprint2

---
+++ src/Mapping.java
@@ -0,0 +1,17 @@
+package ETU1901.framework;
+public class Mapping {
+    String className;
+    String Method;
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethod() {
+        return Method;
+    }
+    public void setMethod(String method) {
+        Method = method;
+    }
+}
